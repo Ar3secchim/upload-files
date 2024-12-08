f56be3b5a21cd1fbe90da787daea2179b39bec69
Renara <renarasecchim@gmail.com>
2024-12-08 07:56:53 -0300

    fix: :bug: update path file lambda fucntion

--- infra/lambda/lambda.tf
+++ infra/lambda/lambda.tf
@@ -5,27 +5,34 @@ variable "s3_bucket_arn" {
 variable "iam_role_arn" {
   description = "ARN da role IAM"
 }
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_dir  = "../app/lambda"
+  output_path = "lambda_handler.zip"
+}
+
 resource "null_resource" "zip_lambda" {
   provisioner "local-exec" {
     command = <<EOT
       cd ../app/lambda && \
-      zip -r ../../infra/lambda_handler.zip .
+      zip -r ../../infra/lambda/lambda_handler.zip .
     EOT
   }
 
   triggers = {
-    # Recria o ZIP se o código mudar
-    source_hash = filebase64sha256("../app/lambda/lambda_function.py")
+    # Recria o ZIP apenas se o código mudar
+    source_hash = data.archive_file.lambda.output_base64sha256
   }
 }
 
 resource "aws_lambda_function" "s3_event_lambda" {
-  filename         = "${path.module}/../../infra/lambda_handler.zip"
-  function_name    = var.lambda_name
-  role             = var.iam_role_arn
-  handler          = "lambda_function.lambda_handler"
+  filename      = "lambda_handler.zip"
+  function_name = var.lambda_name
+  role          = var.iam_role_arn
+  handler       = "lambda_function.lambda_handler"
+
   runtime          = var.lambda_runtime
-  source_code_hash = filebase64sha256("${path.module}/../../infra/lambda_handler.zip")
+  source_code_hash = data.archive_file.lambda.output_base64sha256
 
   depends_on = [null_resource.zip_lambda]
 }
