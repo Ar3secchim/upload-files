7bc7423f85e1f75973e5c1ca0e2cbe5265d8bf0b
Renara <renarasecchim@gmail.com>
2024-12-09 08:07:38 -0300

    feat: :sparkles: add email subscription to SNS topic for notifications

--- infra/sns/sns.tf
+++ infra/sns/sns.tf
@@ -9,3 +9,9 @@ output "sns_topic_arn" {
 output "sns_topic_name" {
   value = aws_sns_topic.s3_event_topic.name
 }
+
+resource "aws_sns_topic_subscription" "email_subscription" {
+  topic_arn = aws_sns_topic.s3_event_topic.arn
+  protocol  = "email"
+  endpoint  = "renarasecchim@gmail.com"
+}
